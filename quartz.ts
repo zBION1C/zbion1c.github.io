e07c590c6ef3856fe1d287789f8fa0a826068308
zBION1C <nmontana745@gmail.com>
2026-07-18 19:49:44 +0200

    Quartz sync: Jul 18, 2026, 7:49 PM

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,17 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
+import { FileTrieNode } from "./quartz/util/fileTrie"
+
+// Advanced: pass callback functions that can't be expressed in YAML
+ExternalPlugin.Explorer({
+  mapFn: (node: FileTrieNode) => {
+    // dont change name of root node
+    if (!node.isFolder) {
+      // set emoji for file/folder      
+        node.displayName = "⊹ " + node.displayName
+    }
+  },
+})
 
 const config = await loadQuartzConfig()
 export default config
